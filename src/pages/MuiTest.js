7c2b0a4e7194e7d1ea1112a16ab8e60ebfff170b
KyosukeYoshino <Yoshino.Kyosuke.82@hopes-ise.co.jp>
2026-05-14 09:21:37 +0900

    first commit

---
+++ src/pages/MuiTest.js
@@ -0,0 +1,14 @@
+import MuiButton from "../Components/MuiButton"
+import MuiThemeTest from "../Components/MuiThemeTest"
+import SignIn from "../Components/SignIn"
+
+const MuiTest = () => {
+
+  return (<>
+    <MuiButton />
+    <MuiThemeTest />
+    <SignIn />
+  </>)
+}
+
+export default MuiTest
